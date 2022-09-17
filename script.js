3813333005fb81f53b6d7b798eb673ac1ae0fcbf
NassCode <63391304+NassCode@users.noreply.github.com>
2022-09-17 14:37:44 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function myFunction () {
+  document.getElementById("demo").innerHTML = 'Hello World!';
+  console.log('Hello World!');
+}
+
+// document.getElementById("demo").onload = myFunction;
+
